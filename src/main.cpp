ec6bbcfd2cdd6a9cbd6e73b3a8e691f3f8bf05df
Diogo Mattioli <diogomattioli@gmail.com>
2025-01-10 13:39:00 +0100

    chore: weak main header

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-int main(int argc, char **argv) __attribute__((weak));
-
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
     std::cout << "Hello world" << std::endl;
